72daa4add8ab14c33c380570a3d4b21ad2225a7d
SUPERMEOWL <supermb397@gmail.com>
2026-08-14 14:35:31 -0300

    HTML | Apuntes Web

---
+++ js/firebaseConfig.js
@@ -0,0 +1,10 @@
+// js/firebaseConfig.js
+export const firebaseConfig = {
+  apiKey: "AIzaSyB_4_Hytf_iSTLgQEe136wjas-eMhjMacM",
+  authDomain: "pasteleria-pedidos.firebaseapp.com",
+  databaseURL: "https://pasteleria-pedidos-default-rtdb.firebaseio.com",
+  projectId: "pasteleria-pedidos",
+  storageBucket: "pasteleria-pedidos.firebasestorage.app",
+  messagingSenderId: "163753170515",
+  appId: "1:163753170515:web:b94dc96a10b99a062ad8a6"
+};
